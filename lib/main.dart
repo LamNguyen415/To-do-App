a8e021f4376b2052aa2724344d8c0d7724208b94
LamNguyen415 <33480810+LamNguyen415@users.noreply.github.com>
2023-12-14 17:09:57 +0900

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,52 @@
+import 'package:flutter/material.dart';
 
+class TimePickerDemo extends StatefulWidget {
+  @override
+  _TimePickerDemoState createState() => _TimePickerDemoState();
+}
+
+class _TimePickerDemoState extends State<TimePickerDemo> {
+  TimeOfDay selectedTime = TimeOfDay.now();
+
+  Future<void> _selectTime(BuildContext context) async {
+    final TimeOfDay? picked = await showTimePicker(
+      context: context,
+      initialTime: selectedTime,
+    );
+    if (picked != null && picked != selectedTime)
+      setState(() {
+        selectedTime = picked;
+      });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Time Picker Demo"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text(
+              "${selectedTime.format(context)}",
+              style: TextStyle(fontSize: 55, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            ElevatedButton(
+              onPressed: () => _selectTime(context),
+              child: Text('Select time'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+void main() => runApp(MaterialApp(
+  home: TimePickerDemo(),
+));
